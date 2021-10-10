06c704409b7034e4b5fa33b1bf30c97fa7eeaeeb
Antonio Lopes <antoniolopesg@protonmail.com>
2021-10-10 14:45:03 -0300

    chore: pre config tests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,5 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.js'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
-  testMatch: ['<rootDir>/__tests__/**/?(*.)+(spec|test).js'],
   testEnvironment: 'jest-environment-node'
 };
